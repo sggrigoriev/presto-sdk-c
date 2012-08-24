3fb7c30a8901cce9675fc94525be4606db95bc64
David Moss <dmoss@peoplepowerco.com>
2012-08-24 12:58:50 -0700

    Output the XML being sent to the server into the syslogs

--- c/iot/xml/generator/iotxmlgen.c
+++ c/iot/xml/generator/iotxmlgen.c
@@ -205,6 +205,7 @@ error_t iotxml_send(char *destMsg, int maxSize) {
         paramTypeMap[lastParamType]);
   }
 
+  SYSLOG_INFO("Send: %s\n", destMsg);
   lastParamType = -1;
   inProgress = false;
   return application_send(destMsg, strlen(destMsg));
